f579ba481154c802338b16a4008da21d8d5a1c06
Kuppili Nikhilesh Raju <138553490+nikhilesh9ix@users.noreply.github.com>
2025-09-19 10:21:01 +0530

    Add DFS function for graph traversal

---
+++ dfs.py
@@ -0,0 +1,21 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    
+    visited.add(node)
+    print(node)
+    
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': [],
+    'E': []
+}
+
+dfs(graph, 'A')
