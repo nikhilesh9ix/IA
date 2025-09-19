752f624cbe2d89029343b7738e376006c9a50260
Kuppili Nikhilesh Raju <138553490+nikhilesh9ix@users.noreply.github.com>
2025-09-19 10:20:27 +0530

    Add BFS algorithm implementation in bfs.py

---
+++ bfs.py
@@ -0,0 +1,24 @@
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+    
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.add(node)
+            print(node)
+            
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+# Example
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': [],
+    'E': []
+}
+
+bfs(graph, 'A')
